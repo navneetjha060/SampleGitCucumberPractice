9a7793392e34989345cb9b3946af291cc3973e7b
navneet <navneet.gullu@gmail.com>
2024-05-21 08:50:32 +0530

    First Commit

---
+++ src/test/java/StepDefenitions/LoginSteps.java
@@ -0,0 +1,36 @@
+package StepDefenitions;
+
+import io.cucumber.java.en.*;
+
+public class LoginSteps {
+	
+	@Given("user is on login page")
+	public void user_is_on_login_page() {
+	    
+	}
+
+	@When("user enters username and password")
+	public void user_enters_username_and_password() {
+	    
+	}
+
+	@And("clicks on login button")
+	public void clicks_on_login_button() {
+	    
+	}
+
+	@Then("user is navigated to home page")
+	public void user_is_navigated_to_home_page() {
+	    
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
