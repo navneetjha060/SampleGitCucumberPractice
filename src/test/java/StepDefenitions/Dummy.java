2b062aa5ddff668f7246d5e87b327b83a0232ecf
Navneet Jha <107983190+navneetjha060@users.noreply.github.com>
2024-06-06 08:42:26 +0530

    Create Dummy.java

---
+++ src/test/java/StepDefenitions/Dummy.java
@@ -0,0 +1,12 @@
+package StepDefenitions;
+
+public class Dummy {
+	
+	
+	public static void dummy1() {
+		System.out.println("This is dummy Linex");
+		System.out.println("This is dummy Liney");
+		System.out.println("This is dummy Linez");
+	}
+
+}
