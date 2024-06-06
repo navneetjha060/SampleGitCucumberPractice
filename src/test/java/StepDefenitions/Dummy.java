5110d2f093c7efe11d5b53b65c500d840fefd6ad
Navneet Jha <107983190+navneetjha060@users.noreply.github.com>
2024-06-06 09:05:46 +0530

    Create Dummy.java

---
+++ src/test/java/StepDefenitions/Dummy.java
@@ -0,0 +1,12 @@
+package StepDefenitions;
+
+public class Dummy {
+	
+	
+	public void dummy() {
+		System.out.println("Dummy4");
+		System.out.println("Dummy5");
+		System.out.println("Dummy6");
+	}
+
+}
