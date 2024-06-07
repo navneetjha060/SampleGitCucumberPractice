24f74e5fece2f3278271a5996649a866c440c8fc
navneet <navneet.gullu@gmail.com>
2024-06-07 08:53:31 +0530

    Changes made today

--- src/test/java/StepDefenitions/Dummy.java
+++ src/test/java/StepDefenitions/Dummy.java
@@ -7,6 +7,10 @@ public class Dummy {
 		System.out.println("Dummy4");
 		System.out.println("Dummy5");
 		System.out.println("Dummy6");
+		
+		System.out.println("Dummy1");
+		System.out.println("Dummy2");
+		System.out.println("Dummy3");
 	}
 
 }
